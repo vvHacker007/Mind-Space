404f9345d1c7740fc95cc599b0480a459f6ef784
Vedansh Vijaywargiya <60468275+vvHacker007@users.noreply.github.com>
2021-02-03 01:36:39 +0530

    Added User Checking Feature

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask,redirect,request,url_for,jsonify,session,render_template
+from flask import Flask,redirect,request,url_for,jsonify,session,render_template,flash
 import pymongo as pm
 
 app = Flask(__name__)
-
 client = pm.MongoClient("mongodb+srv://Blogger:vedv2002@cluster0.krw6c.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = client.Blogs
 login_col = db.Login
@@ -11,8 +10,22 @@ data = None
 @app.route('/')
 def redirect_to_home():
     return render_template("home.html")
+
 @app.route('/login/',methods=["POST","GET"])
 def login():
+    if request.method == "POST":
+        user_name = request.form["user_name"]
+        password = request.form["pass"]
+        if login_col.find_one({'User Name': user_name}):
+            print("Found USER!!!!!!!!!!!!!!!!!!!!!!!!")
+            return redirect(url_for("user", name_u = user_name))
+        else:
+            print("NOT Found USER!!!!!!!!!!!!!!!!!!!!!!!!")
+            flash(f"Login Unsuccessfull")
+            return render_template("login.html")
+    
+    else:
+        return render_template("login.html")
     """
     if request.method == "POST":
         check_email = request.form["id"]
@@ -24,7 +37,7 @@ def login():
             return render_template("signup.html")
     else:
         """
-    return render_template("login.html")
+    
 
 @app.route('/signup/',methods=["POST","GET"])
 def signup():
@@ -44,9 +57,10 @@ def signup():
     return render_template("signup.html")
         
 
-@app.route('/user/')
-def user():
-    return render_template("user.html")
+@app.route('/<name_u>/')
+def user(name_u):
+    return render_template("user.html", name_u=name_u)
 
 if __name__ == '__main__':
+    app.secret_key = "hsagfgfahg"
     app.run(debug=True)
