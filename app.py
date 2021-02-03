599d83bae9ea6f071d1e110ee9be2701163ef152
Vedansh Vijaywargiya <60468275+vvHacker007@users.noreply.github.com>
2021-02-03 23:35:43 +0530

    Added signup verification

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask,redirect,request,url_for,jsonify,session,render_template,flash
 import pymongo as pm
+import datetime
+from dateutil.relativedelta import relativedelta, MO
+import phonenumbers
+import pytz
 
 app = Flask(__name__)
 client = pm.MongoClient("mongodb+srv://Blogger:vedv2002@cluster0.krw6c.mongodb.net/<dbname>?retryWrites=true&w=majority")
 db = client.Blogs
 login_col = db.Login
 data = None
+name=None
+phone=None
+
+ist = pytz.timezone('Asia/Kolkata')
 
 @app.route('/')
 def redirect_to_home():
@@ -34,20 +42,42 @@ def forgot_pass():
 
 @app.route('/signup/',methods=["POST","GET"])
 def signup():
-    """
     if request.method == "POST":
-        name = request.form["name"]
         user_name = request.form["username"]
-        email = request.form["id"]
-        password = request.form["pass"]
-        age = request.form["age"]
+        email = request.form["emailid"]
         phone = request.form["phone"]
-        mydict = {"Name":name,"User Name":user_name,"Email":email,"Password":password,"Age":age,"Phone":phone}
+        password = request.form["pass"]
+        check_password = request.form["confirm_pass"]
+        birthdate = request.form["birthdate"]
+        y,m,d = birthdate.split("-")
+        
+        if (login_col.find_one({'User Name': user_name})):
+            flash(f"Username already exists")
+            return render_template("signup.html")
+        
+        if(password!=check_password):
+            flash(f"Password doesn't match!")
+            return render_template("signup.html")
+        
+        if (login_col.find_one({'Email': email})):
+            flash(f"Account with same Email-id already exists!")
+            return render_template("signup.html")
+        
+        name = user_name
+
+        date_today = str(datetime.date.today())
+        ty,tm,td = date_today.split("-")
+        date1 = datetime.datetime(int(y),int(m),int(d))
+        date2 = datetime.datetime(int(ty),int(tm),int(td))
+        years = relativedelta(date2,date1).years
+        if phone != None:
+            phonenumbers.parse(phone,"")
+
+        mydict = {"Name":name,"User Name":user_name,"Email":email,"Password":password,"Age":years,"Phone":phone}
         login_col.insert_one(mydict)
-        return render_template("user.html")
+        return render_template("login.html")
     else:
-        """
-    return render_template("signup.html")
+        return render_template("signup.html")
         
 
 @app.route('/<name_u>/')
@@ -56,4 +86,6 @@ def user(name_u):
 
 if __name__ == '__main__':
     app.secret_key = "hsagfgfahg"
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
