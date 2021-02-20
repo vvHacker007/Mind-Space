7e691aa9aa0a97e70208a7559adefc2b5ae83814
sg23600 <sujaygoswami@hotmail.com>
2021-02-20 23:36:53 +0530

    added cross icon for searchbox

--- Sujay/Mind-Space-React/src/components/Searchbox.js
+++ Sujay/Mind-Space-React/src/components/Searchbox.js
@@ -1,27 +1,43 @@
 import React from "react"
 import { TextField, IconButton } from "material-ui"
 import SearchIcon from "material-ui/svg-icons/action/search"
+import cross from "./cross.png"
+//import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
 const SearchBox = ({ isOpen, onClick, additionalStyles }) => {
   const baseStyles = {
     open: {
-      width: 300,
+      width: "180px",
     },
     closed: {
-      width: 0,
+      width: "0px",
     },
-    smallIcon: {
+    smallsearchIcon: {
       width: 30,
       height: 30,
     },
     icon: {
-      width: 40,
-      height: 40,
-      padding: 5,
-      top: 10,
+      position: "absolute",
+      right: 0,
+      height: "10px",
+      width: "10px",
+      background: "transparent",
+      textAlign: "center",
+      lineHeight: "35px",
+      fontSize: "1em",
+      color: "Black",
+      cursor: "pointer",
+      zIndex: 1,
+      top: "-3px",
+      transition: "all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55)",
     },
     frame: {
-      border: "solid 1px black",
+      border: "solid 0px black",
       borderRadius: 5,
+      position: "relative",
+      height: "35px",
+      width: "35px",
+      transition: "all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55)",
+      top: "-8px",
     },
   }
 
@@ -39,16 +55,24 @@ const SearchBox = ({ isOpen, onClick, additionalStyles }) => {
   )
   divStyle.width += baseStyles.icon.width + 5
 
+  let iconDisplay = () => {
+    if (!isOpen) {
+      return <SearchIcon />
+    } else return <img src={cross} alt="X" />
+  }
+
   return (
     <div style={divStyle}>
       <IconButton
-        iconStyle={baseStyles.smallIcon}
+        iconStyle={baseStyles.smallsearchIcon}
         style={baseStyles.icon}
         onClick={() => onClick()}
       >
-        <SearchIcon />
+        {iconDisplay()}
+        {/* <SearchIcon />
+        <img src={cross} alt="X" /> */}
       </IconButton>
-      <TextField name="search" style={textStyle} />
+      <TextField name="Search..." style={textStyle} />
     </div>
   )
 }
--- Sujay/Mind-Space-React/src/containers/Home/Home.js
+++ Sujay/Mind-Space-React/src/containers/Home/Home.js
@@ -75,6 +75,29 @@ class Home extends Component {
             </div>
           </div>
         </nav>
+        <div className="home-head">
+          <div class="container h-100">
+            <div class="row h-100 align-items-center justify-content-center text-center">
+              <div class="col-lg-10 align-self-end">
+                <h1 class="text-uppercase text-white font-weight-bold">
+                  Mind Space
+                </h1>
+                <hr class="divider my-4" />
+              </div>
+              <div class="col-lg-8 align-self-baseline">
+                <p class="text-white-75 font-weight-light mb-5">
+                  A mint of Creativity...
+                </p>
+                <a
+                  class="btn btn-primary btn-xl js-scroll-trigger"
+                  href="#about"
+                >
+                  Find Out More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
