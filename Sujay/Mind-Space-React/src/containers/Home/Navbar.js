281d7b205f414417dca1c94a34f4d42723608f0f
sg23600 <sujaygoswami@hotmail.com>
2021-03-06 22:02:29 +0530

    revamp

--- Sujay/Mind-Space-React/src/containers/Home/Navbar.js
+++ Sujay/Mind-Space-React/src/containers/Home/Navbar.js
@@ -14,6 +14,7 @@ const Navbar = () => {
         <div className="logo-container">
           <a href="#">
             <Logo className="logo" />
+            <p>Mind Space</p>
           </a>
         </div>
         <ul className={click ? "nav-options active" : "nav-options"}>
@@ -21,13 +22,13 @@ const Navbar = () => {
             <a href="#">ABOUT</a>
           </li>
           <li className="option" onClick={closeMobileMenu}>
-            <a href="#">CONTACT</a>
+            <a href="#">SERVICES</a>
           </li>
           <li className="option" onClick={closeMobileMenu}>
-            <a href="#">BLOG</a>
+            <a href="#">STORIES</a>
           </li>
           <li className="option mobile-option" onClick={closeMobileMenu}>
-            <a href="#">SIGN-IN</a>
+            <a href="#">CONTACT</a>
           </li>
           <li className="option mobile-option" onClick={closeMobileMenu}>
             <a href="" className="sign-up">
@@ -38,7 +39,7 @@ const Navbar = () => {
       </div>
       <ul className="signin-up">
         <li className="sign-in" onClick={closeMobileMenu}>
-          <a href="#">SIGN-IN</a>
+          <a href="#">CONTACT</a>
         </li>
         <li onClick={closeMobileMenu}>
           <a href="" className="signup-btn">
